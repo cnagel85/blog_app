45e523b65ca0249051094add3482ec048e89962f
Corey Nagel <cnagel85@hotmail.com>
2013-04-13 08:23:40 -0600

    some layout fixes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,16 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  private
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
 end
